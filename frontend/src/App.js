b89ffaa822615421cab421f90501542057c95b64
heuzin <matheusqs.arq@gmail.com>
2021-05-26 17:34:51 -0400

    header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Bem Vindos a Flor & Arte</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Bem Vindos a Flor&Arte</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
