7ef923054da54b93c1ae7b129d12ff98e7d89d19
heuzin <matheusqs.arq@gmail.com>
2021-05-28 13:05:48 -0400

    serving products - bacn end routes

---
+++ backend/data/products.js
@@ -0,0 +1,112 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Brilhante',
+        image: '/images/pulseira1.jpg',
+        description: 'Variedades de pulseirinhas personalizadas, escolha a sua',
+        bramd: 'Flor&Arte',
+        category: 'Pulseiras',
+        price: 50.00,
+        countInStock: 4,
+        rating: 4.5,
+        numReviews: 4
+    }, 
+    {
+        _id: '2',
+        name: 'Colorida',
+        image: '/images/pulseira2.jpg',
+        description: 'Variedades de pulseirinhas personalizadas, escolha a sua',
+        bramd: 'Flor&Arte',
+        category: 'Pulseiras',
+        price: 40.00,
+        countInStock: 2,
+        rating: 3.5,
+        numReviews: 6
+    }, 
+    {
+        _id: '3',
+        name: 'Personalizada',
+        image: '/images/pulseira3.jpg',
+        description: 'Variedades de pulseirinhas personalizadas, escolha a sua',
+        bramd: 'Flor&Arte',
+        category: 'Pulseiras',
+        price: 30.00,
+        countInStock: 1,
+        rating: 2.5,
+        numReviews: 2
+    }, 
+    {
+        _id: '4',
+        name: 'Brinco',
+        image: '/images/brinco1.jpg',
+        description: 'Variedades de brincos personalizados, escolha o seu',
+        bramd: 'Flor&Arte',
+        category: 'Brincos',
+        price: 10.00,
+        countInStock: 4,
+        rating: 4.5,
+        numReviews: 4
+    }, 
+    {
+        _id: '5',
+        name: 'Pavao',
+        image: '/images/brinco2.jpg',
+        description: 'Variedades de brincos personalizados, escolha o seu',
+        bramd: 'Flor&Arte',
+        category: 'Brincos',
+        price: 60.00,
+        countInStock: 10,
+        rating: 5.0,
+        numReviews: 1
+    }, 
+    {
+        _id: '6',
+        name: 'Cai Orelha',
+        image: '/images/pulseira1.jpg',
+        description: 'Variedades de brincos personalizados, escolha o seu',
+        bramd: 'Flor&Arte',
+        category: 'Brincos',
+        price: 50.00,
+        countInStock: 0,
+        rating: 0,
+        numReviews: 0 
+    } ,
+    {
+        _id: '7',
+        name: 'Colar',
+        image: '/images/colar1.jpg',
+        description: 'Variedades de colares personalizados, escolha o seu',
+        bramd: 'Flor&Arte',
+        category: 'Pulseiras',
+        price: 50.00,
+        countInStock: 4,
+        rating: 4.5,
+        numReviews: 4
+    },
+    {
+        _id: '8',
+        name: 'Quebra Pescoco',
+        image: '/images/colar2.jpg',
+        description: 'Variedades de colares personalizados, escolha o seu',
+        bramd: 'Flor&Arte',
+        category: 'Colares',
+        price: 100.00,
+        countInStock: 2,
+        rating: 1.5,
+        numReviews: 3
+    },
+    {
+        _id: '9',
+        name: 'Torce Pescoco',
+        image: '/images/colar3.jpg',
+        description: 'Variedades de colares personalizados, escolha o seu',
+        bramd: 'Flor&Arte',
+        category: 'Colares',
+        price: 25.00,
+        countInStock: 6,
+        rating: 3.0,
+        numReviews: 7
+    },
+]
+
+module.exports = products
