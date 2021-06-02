b655e4ffa448e3e59170dd93ef5e85724c4ad111
heuzin <matheusqs.arq@gmail.com>
2021-06-02 17:03:48 -0400

    user register reducer action and screen

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -5,16 +5,19 @@ import  { useDispatch, useSelector } from 'react-redux'
 import  Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { login } from '../actions/userActions'
+import { register } from '../actions/userActions'
 
-const LoginScreen = ({ history, location }) => {
+const RegisterScreen = ({ history, location }) => {
+    const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const[password, setPassword] = useState('')
+    const[confirmPassword, setConfirmPassword] = useState('')
+    const[message, setMessage] = useState(null)
 
     const dispatch = useDispatch()
 
-    const userLogin = useSelector(state => state.userLogin)
-    const { loading, error, userInfo } = userLogin
+    const userRegister = useSelector(state => state.userRegister)
+    const { loading, error, userInfo } = userRegister
 
     const redirect = location.search ? location.search.split('=')[1] : '/'
 
@@ -26,17 +29,31 @@ const LoginScreen = ({ history, location }) => {
 
     const submitHandler = (e) =>  {
         e.preventDefault()
-        // DISPATCH LOGIN
-        dispatch(login(email, password))
+        // DISPATCH REGISTER
+        if(password !== confirmPassword) {
+            setMessage('Passwords nao combinam')
+        } else {
+            dispatch(register(name, email, password))
+        }
     }
 
     return (
         <FormContainer>
-            <h1>Entrar</h1>
+            <h1>Criar Conta</h1>
+            {message && <Message variant='danger'>{message}</Message>}
             {error && <Message variant='danger'>{error}</Message>}
             {loading && <Loader />}
             <Form onSubmit={submitHandler}>
-                <Form.Group controlId='email'>
+                <Form.Group controlId='name'>
+                    <Form.Label>Nome</Form.Label>
+                    <Form.Control 
+                        type='name' 
+                        placeholder='Nome' 
+                        value={name} 
+                        onChange={(e) => setName(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Form.Group  style={{ paddingTop: '10px' }} controlId='email'>
                     <Form.Label>Endereço de Email</Form.Label>
                     <Form.Control 
                         type='email' 
@@ -54,16 +71,25 @@ const LoginScreen = ({ history, location }) => {
                         onChange={(e) => setPassword(e.target.value)}
                     ></Form.Control>
                 </Form.Group>
+                <Form.Group style={{ paddingTop: '10px' }} controlId='confirmPassword'>
+                    <Form.Label>Confirmar Password</Form.Label>
+                    <Form.Control 
+                        type='password' 
+                        placeholder='Confirmar Password' 
+                        value={confirmPassword} 
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
 
                 <Button style={{ marginTop: '10px' }} type='submit' variant='primary'>
-                    Entrar
+                    Registrar
                 </Button>
             </Form>
 
             <Row className='py-3'>
                 <Col>
-                    Novo Usuario? <Link to={redirect  ? `/register?redirect=${redirect}` : '/register'}>
-                        Criar Conta
+                    Tem Uma Conta? <Link to={redirect  ? `/login?redirect=${redirect}` : '/login'}>
+                        Entrar
                     </Link>
                 </Col>
             </Row>
@@ -71,4 +97,4 @@ const LoginScreen = ({ history, location }) => {
     )
 }
 
-export default LoginScreen
+export default RegisterScreen
