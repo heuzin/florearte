24cae34efbade0b9336aabe3e78deb7780bc4845
heuzin <matheusqs.arq@gmail.com>
2021-06-07 18:00:10 -0400

    front end image upload

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
 import  { useDispatch, useSelector } from 'react-redux'
@@ -18,6 +19,7 @@ const ProductEditScreen = ({ history, match }) => {
     const [category, setCategory] = useState('')
     const [countInStock, setCountInStock] = useState(0)
     const [description, setDescription] = useState('')
+    const [uploading, setUploading] = useState(false)
 
     const dispatch = useDispatch()
 
@@ -46,6 +48,29 @@ const ProductEditScreen = ({ history, match }) => {
         }
         
     }, [dispatch, history, product, productId, successUpdate])
+
+    const uploadFileHandler = async (e) => {
+        const file = e.target.files[0]
+        const formData = new FormData()
+        formData.append('image', file)
+        setUploading(true)
+
+        try {
+            const config ={
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+
+            const { data } = await axios.post('/api/upload', formData, config)
+
+            setImage(data)
+            setUploading(false)
+        } catch (error) {
+            console.error(error)
+            setUploading(false)
+        }
+    }
 
     const submitHandler = (e) =>  {
         e.preventDefault()
@@ -99,6 +124,12 @@ const ProductEditScreen = ({ history, match }) => {
                                 value={image} 
                                 onChange={(e) => setImage(e.target.value)}
                             ></Form.Control>
+                            <Form.File 
+                                id='image-file' 
+                                label='Escolha arquivo' 
+                                custom onChange={uploadFileHandler}
+                            ></Form.File>
+                            {uploading && <Loader />}
                         </Form.Group>
                         <Form.Group style={{ paddingTop: '10px' }} controlId='brand'>
                             <Form.Label>Marca</Form.Label>
